6c9ba072df348b2c449de75ee57d96045a7cc2af
Robel Amde Kirkos <90899830+Robel-Ak@users.noreply.github.com>
2023-03-23 20:15:59 +0000

    Responsive Navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import "./styles.css";
 import Navbar from "./components/NavbarComponents/Navbar";
+import { Routes } from "react-router-dom";
 export default function App() {
   return (
     <div className="App">
+      {/* <Routes>
+        <Navbar/>
+      </Routes> */}
       <Navbar/>
     </div>
   );
--- src/components/NavbarComponents/Navbar.js
+++ src/components/NavbarComponents/Navbar.js
@@ -1,15 +1,31 @@
 import { Component } from "react";
 import "./NavbarStyles.css";
-
-class Navbar extends Component{
-    render(){
-        return(
+import { MenuItems } from "../MenuItems";
+import { Link } from "react-router-dom";
+class Navbar extends Component {
+    state = {clicked : false};
+    handleClick = () => {
+        this.setState({
+            clicked: !this.state.clicked
+        })
+    }
+    render() {
+        return (
             <nav className="NavbarItems">
-                <h1 className="navbar-logo">SAM</h1>
-                <ul className="nav-menu">
-                    <li>
-                       <Link><i class="fa-solid fa-house"></i>Home</Link>
-                    </li>
+                <h1 className="navbar-logo">AA-SAM</h1>
+                <div className="menu-icons" onClick={this.handleClick}>
+                    <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
+                </div>
+                <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
+                    {MenuItems.map((item, index) => {
+                        return (
+                            <li key={index}>
+                                {/* <Link><i class="fa-solid fa-house"></i>Home</Link> */}
+                                <a className={item.cName} href="/"><i className={item.icon}></i>{item.title}</a>
+                            </li>
+                        );
+                    })}
+
                 </ul>
             </nav>
         )
